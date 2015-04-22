04f602b5a0d083803169e91962b2e08835a5d8d0
Thijs <jemoederopeenbromfiets@live.nl>
2015-04-22 10:58:21 +0200

    Als een baas

--- src/pp/block1/cc/dfa/CheckerImplementation.java
+++ src/pp/block1/cc/dfa/CheckerImplementation.java
@@ -5,8 +5,11 @@ public class CheckerImplementation implements Checker{
 	@Override
 	public boolean accepts(State start, String word) {
 		State ID6_DFA = State.ID6_DFA;
-		for(int index = 0; index < word.length(); index++)
-			ID6_DFA = ID6_DFA.getNext(word.charAt(index));
+		for(int index = 0; index < word.length(); index++){
+			if(ID6_DFA.hasNext(word.charAt(index)))
+				ID6_DFA = ID6_DFA.getNext(word.charAt(index));
+			else return false;
+		}
 		return ID6_DFA.isAccepting();
 	}
 
