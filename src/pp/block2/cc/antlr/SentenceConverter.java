62f2c57ecc41bd120f4806b8136ff2b66c76b8e2
Thijs <jemoederopeenbromfiets@live.nl>
2015-05-06 10:58:34 +0200

    SentenceConverter

--- src/pp/block2/cc/antlr/SentenceConverter.java
+++ src/pp/block2/cc/antlr/SentenceConverter.java
@@ -1,5 +1,8 @@
 package pp.block2.cc.antlr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.antlr.v4.runtime.Lexer;
 
 import pp.block2.cc.AST;
@@ -29,7 +32,7 @@ public class SentenceConverter //
 		extends SentenceBaseListener implements Parser {
 	public SentenceConverter() {
 		this.fact = new SymbolFactory(Sentence.class);
-		
+		this.astList = new ArrayList<AST>();
 	}
 
 	/** Factory needed to create terminals of the {@link Sentence}
@@ -39,50 +42,59 @@ public class SentenceConverter //
 
 	@Override
 	public AST parse(Lexer lexer) {
+		astList.clear();
 		SentenceParser parser = new SentenceParser(new CommonTokenStream(lexer));
 		ParseTree tree = parser.sentence();
 		new ParseTreeWalker().walk(this, tree);
-		result = new AST(SENT);
-		// Fill in
 		return result;
 	}
 	
 	@Override
 	public void exitSentence(SentenceContext ctx) {
-		AST ast = new AST(SENT);
-		
+		result = new AST(SENT);
+		for(AST ast : astList){
+			result.addChild(ast);
+		}
 	}
 
 	@Override
 	public void exitSubject(SubjectContext ctx) {
 		AST ast = new AST(SUBJ);
+		ast.addChild(astList.get(astList.size()-1));
+		astList.set(astList.size()-1, ast);	
 	}
 
 	@Override
 	public void exitModifier(ModifierContext ctx) {
 		AST ast = new AST(MOD);
+		ast.addChild(astList.get(astList.size()-1));
+		astList.set(astList.size()-1, ast);
 	}
 
 	@Override
 	public void exitObject(ObjectContext ctx) {
 		AST ast = new AST(OBJ);
+		ast.addChild(astList.get(astList.size()-1));
+		astList.set(astList.size()-1, ast);
 	}
 
 	@Override
 	public void visitTerminal(TerminalNode node) {
+		astList.add(new AST(fact.getTerminals().get(node.getSymbol().getType()), node.getSymbol()));
 	}
 
 	@Override
 	public void visitErrorNode(ErrorNode node) {
 	}
 	
-	private static final NonTerm SENT = new NonTerm("sentence");
-	private static final NonTerm SUBJ = new NonTerm("subject");
-	private static final NonTerm MOD = new NonTerm("modifier");
-	private static final NonTerm OBJ = new NonTerm("object");
+	private static final NonTerm SENT = new NonTerm("Sentence");
+	private static final NonTerm SUBJ = new NonTerm("Subject");
+	private static final NonTerm MOD = new NonTerm("Modifier");
+	private static final NonTerm OBJ = new NonTerm("Object");
 	
 	
 	private AST result; 
+	private List<AST> astList;
 	
 	// From here on overwrite the listener methods
 	// Use an appropriate ParseTreeProperty to
