86e5f537ba46666f121dd45fef3d0c631d5ec9da
anirudh55 <anirudh.ksg@gmail.com>
2015-05-06 09:54:07 +0200

    sentence converter

--- src/pp/block2/cc/antlr/SentenceConverter.java
+++ src/pp/block2/cc/antlr/SentenceConverter.java
@@ -1,5 +1,12 @@
 package pp.block2.cc.antlr;
 
+import org.antlr.v4.runtime.Lexer;
+
+import pp.block2.cc.AST;
+import pp.block2.cc.Parser;
+import pp.block2.cc.SymbolFactory;
+import pp.block2.cc.ll.Sentence;
+
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
@@ -16,14 +23,20 @@ import pp.block2.cc.antlr.SentenceParser.ObjectContext;
 import pp.block2.cc.antlr.SentenceParser.SentenceContext;
 import pp.block2.cc.antlr.SentenceParser.SubjectContext;
 
-public class SentenceConverter extends SentenceBaseListener implements Parser{
+public class SentenceConverter //
+		extends SentenceBaseListener implements Parser {
+	public SentenceConverter() {
+		this.fact = new SymbolFactory(Sentence.class);
+	}
+
+	/** Factory needed to create terminals of the {@link Sentence}
+	 * grammar. See {@link pp.block2.cc.ll.SentenceParser} for
+	 * example usage. */
+	private final SymbolFactory fact;
 
 	@Override
-	public AST parse(Lexer lexer) throws ParseException {
-		SentenceParser parser = new SentenceParser(new CommonTokenStream(lexer));
-		ParseTree tree = parser.sentence();
-		System.out.println(tree.toStringTree(parser));
-		return null;
+	public AST parse(Lexer lexer) {
+		// Fill in
 	}
 	
 	@Override
@@ -53,5 +66,7 @@ public class SentenceConverter extends SentenceBaseListener implements Parser{
 	}
 	
 	private static final NonTerm SENT = new NonTerm("sentence");
-	
+	// From here on overwrite the listener methods
+	// Use an appropriate ParseTreeProperty to
+	// store the correspondence from nodes to ASTs
 }
