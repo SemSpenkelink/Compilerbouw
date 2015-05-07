1d98417d5678ae0fa7c3b82f07de009077618013
anirudh55 <anirudh.ksg@gmail.com>
2015-05-07 10:58:28 +0200

    grammar3

---
+++ src/pp/huiswerk/pp/huiswerk/grammar3Listener.java
@@ -0,0 +1,21 @@
+// Generated from grammar3.g4 by ANTLR 4.4
+package pp.huiswerk;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link grammar3Parser}.
+ */
+public interface grammar3Listener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link grammar3Parser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(@NotNull grammar3Parser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link grammar3Parser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(@NotNull grammar3Parser.RContext ctx);
+}
