81e7b5fa960fbfe06f38e99c58ffa6cfc8033928
anirudh55 <anirudh.ksg@gmail.com>
2015-05-19 11:26:45 +0200

    listerner bottom up

--- src/pp/block4/cc/cfg/BottomUpCFGBuilder.java
+++ src/pp/block4/cc/cfg/BottomUpCFGBuilder.java
@@ -10,14 +10,24 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 import pp.block4.cc.ErrorListener;
 
 /** Template bottom-up CFG builder. */
 public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** The CFG being built. */
 	private Graph graph;
-
+	
+	ParseTreeProperty<Node> entry = new ParseTreeProperty<Node>();
+	ParseTreeProperty<Node> exit = new ParseTreeProperty<Node>();
+	
+	int counter = 1; 
+	
 	/** Builds the CFG for a program contained in a given file. */
 	public Graph build(File file) {
 		Graph result = null;
@@ -49,7 +59,7 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** Builds the CFG for a program given as an ANTLR parse tree. */
 	public Graph build(ParseTree tree) {
 		this.graph = new Graph();
-		// Fill in
+		
 	}
 
 	/** Adds a node to he CGF, based on a given parse tree node.
@@ -73,4 +83,75 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 			System.out.println(builder.build(file));
 		}
 	}
+	
+	public void awesomeMethod(ParseTree ctx){
+		Node tmpEntry = new Node(counter); counter++;
+		Node tmpExit = new Node(counter);	counter++;
+			for(int i = 0; i < ctx.getChildCount();i++){
+				tmpEntry.addEdge(entry.get(ctx.getChild(i)));
+				entry.get(ctx.getChild(i)).addEdge(tmpExit);
+			}
+		entry.put(ctx, tmpEntry);
+		exit.put(ctx, tmpExit);
+	}
+	
+	@Override public void exitBlockStat(@NotNull FragmentParser.BlockStatContext ctx) {
+		awesomeMethod(ctx);
+	}
+
+	@Override public void exitContStat(@NotNull FragmentParser.ContStatContext ctx) {awesomeMethod(ctx); }
+	
+	@Override public void exitDecl(@NotNull FragmentParser.DeclContext ctx) {awesomeMethod(ctx); }
+
+	@Override public void exitPrintStat(@NotNull FragmentParser.PrintStatContext ctx) {awesomeMethod(ctx); }
+	
+	@Override public void exitProgram(@NotNull FragmentParser.ProgramContext ctx) {awesomeMethod(ctx); }
+	
+	@Override public void exitType(@NotNull FragmentParser.TypeContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void exitParExpr(@NotNull FragmentParser.ParExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitArrayTarget(@NotNull FragmentParser.ArrayTargetContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitCompExpr(@NotNull FragmentParser.CompExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitBitAndExpr(@NotNull FragmentParser.BitAndExprContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void exitWhileStat(@NotNull FragmentParser.WhileStatContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void exitIfStat(@NotNull FragmentParser.IfStatContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitIdTarget(@NotNull FragmentParser.IdTargetContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitArrayExpr(@NotNull FragmentParser.ArrayExprContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void exitInExpr(@NotNull FragmentParser.InExprContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void exitOrExpr(@NotNull FragmentParser.OrExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitBreakStat(@NotNull FragmentParser.BreakStatContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitFieldExpr(@NotNull FragmentParser.FieldExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitBitOrExpr(@NotNull FragmentParser.BitOrExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitNotExpr(@NotNull FragmentParser.NotExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitAddExpr(@NotNull FragmentParser.AddExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitAssignStat(@NotNull FragmentParser.AssignStatContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitConstExpr(@NotNull FragmentParser.ConstExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitIdExpr(@NotNull FragmentParser.IdExprContext ctx) { awesomeMethod(ctx);}
+	
+	@Override public void exitAndExpr(@NotNull FragmentParser.AndExprContext ctx) { awesomeMethod(ctx);}
+
+	@Override public void visitTerminal(@NotNull TerminalNode node) {
+		Node tmp = new Node(counter);
+		entry.put(node, tmp);
+		exit.put(node, tmp);
+	}
+
+	@Override public void visitErrorNode(@NotNull ErrorNode node) { }
 }
