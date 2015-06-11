e309e452ac5dbf1690f3de6300e2b3c108cea883
anirudh55 <anirudh.ksg@gmail.com>
2015-06-11 11:38:25 +0200

    CFG crap

--- src/pp/homework/q2_2/ILOC2CFG.java
+++ src/pp/homework/q2_2/ILOC2CFG.java
@@ -2,13 +2,22 @@ package pp.homework.q2_2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import pp.iloc.Assembler;
+import pp.iloc.model.Label;
+import pp.iloc.model.Op;
+import pp.iloc.model.OpCode;
+import pp.iloc.model.Operand;
 import pp.iloc.model.Program;
 import pp.iloc.parse.FormatException;
 
 public class ILOC2CFG {
 	/** The singleton instance of this class. */
 	private static final ILOC2CFG instance = new ILOC2CFG();
+	 
 
 	/** Returns the singleton instance of this class. */
 	public static ILOC2CFG instance() {
@@ -36,7 +45,58 @@ public class ILOC2CFG {
 	}
 
 	public Graph convert(Program prog) {
-		// TODO: Fill in
-		throw new UnsupportedOperationException();
+		Graph graph = new Graph();
+		Node currentNode = null;
+		Map <Label,Node> labels = new HashMap<Label,Node>();
+		List <Operand> operands; 
+		Op current;
+		int size = prog.size();
+		for(int index = 0; index < size; index++){
+			current = prog.getOpAt(index);
+			
+			//Add and check label//
+			if(current.hasLabel() && !labels.containsKey(current.getLabel())){
+				currentNode = graph.addNode(current.getLabel().toString());
+				labels.put(current.getLabel(),currentNode);
+			}else if(current.hasLabel()){
+				currentNode = labels.get(current.getLabel());
+			}
+			
+			if(current.getOpCode().equals(OpCode.jumpI)){
+				operands = current.getOpnds();
+				Label tmp;
+				for(int i = 0; i < operands.size(); i++){
+					if(operands.get(i).getType().equals(Operand.Type.LABEL)){
+						tmp = (Label)operands.get(i);
+						if (labels.containsKey(tmp)){
+							currentNode.addEdge(labels.get(tmp));
+						}
+						else{							
+							labels.put(tmp, graph.addNode(tmp.toString()));
+							currentNode.addEdge(labels.get(tmp));
+						}
+					}					
+				}
+			} else	if(current.getOpCode().equals(OpCode.cbr)){
+				operands = current.getOpnds();
+				Label tmp;
+				for(int i = 0; i < operands.size(); i++){
+					if(operands.get(i).getType().equals(Operand.Type.LABEL)){
+						tmp = (Label)operands.get(i);
+						if (labels.containsKey(tmp)){
+							currentNode.addEdge(labels.get(tmp));
+						}
+						else{							
+							labels.put(tmp, graph.addNode(tmp.toString()));
+							currentNode.addEdge(labels.get(tmp));
+						}
+					}					
+				}
+			}
+
+			
+		}
+		return graph;
+		//throw new UnsupportedOperationException();
 	}
 }
