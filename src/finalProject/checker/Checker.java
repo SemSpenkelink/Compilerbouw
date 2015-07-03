9e2fa68a022b71d08741abb8156efdc3f75fe1a4
Thijs <jemoederopeenbromfiets@live.nl>
2015-07-03 12:15:02 +0200

    Checker update

--- src/finalProject/checker/Checker.java
+++ src/finalProject/checker/Checker.java
@@ -537,7 +537,7 @@ public class Checker extends EloquenceBaseListener {
 	 * Link the exit node of functionID to the exit node of statVoid.
 	 */
 	@Override public void exitStatVoid(finalProject.grammar.EloquenceParser.StatVoidContext ctx) {
-		setType(ctx, null);
+		setType(ctx, this.scope.type(ctx.functionID().getText()));
 
 		/** CFG creation. */
 		constructNodes(ctx);
